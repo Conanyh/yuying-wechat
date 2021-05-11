08831b0b2e8a88e4b7d69e59e996b49ee07822f9
duheyuan <17696711092@163.com>
2021-05-11 14:21:04 +0800

    feat: 页面样式调整

--- pages/score/score.js
+++ pages/score/score.js
@@ -12,21 +12,21 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     var that = this;
     that.getScore()
   },
@@ -34,48 +34,48 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
-  detail: function () {
+  detail: function(e) {
+    let id = e.currentTarget.dataset.id
     wx.navigateTo({
-      url: '/pages/detail/detail',
+      url: '/pages/detail/detail?id=' + id,
     })
   },
 
-  getScore: function () {
+  getScore: function() {
     var that = this;
     var openid = wx.getStorageSync('openid');
-    console.log(openid)
     wx.request({
       url: app.globalData.host + '/user/getMyGrade',
       data: {
@@ -85,7 +85,7 @@ Page({
       header: {
         "Content-Type": "application/json"
       },
-      success: function (res) {
+      success: function(res) {
         console.log(res)
         if (res.data.code) {
           that.setData({
